cb00b7b836587049636a8897b870ce6752f2b00c
scott813813 <93747891+scott813813@users.noreply.github.com>
2023-02-13 18:05:56 -0800

    Update step_response_plotter.py

--- src/step_response_plotter.py
+++ src/step_response_plotter.py
@@ -4,7 +4,7 @@ This file contains code which takes in time and position data from the serial po
 and outputs plots of the time and position
 
 @author mecha12
-@date   31-jan
+@date   13-Feb-2023
 """
 
 import serial # Import serial library
@@ -18,11 +18,11 @@ mot_2_x_data = []
 mot_2_y_data = []
 
 # Create labels for plots
-mot_1_x_label = 'M1: Time, ms'  ## Variable name
-mot_1_y_label = 'M1: Location, ticks'  ## Variable name
+mot_1_x_label = 'M1: Time, ms'  # Motor 1, x-axis title
+mot_1_y_label = 'M1: Location, ticks'  # Motor 1, y-axis title
 
-mot_2_x_label = 'M2: Time, ms'  ## Variable name
-mot_2_y_label = 'M2: Location, ticks'  ## Variable name
+mot_2_x_label = 'M2: Time, ms'  # Motor 2, x-axis title
+mot_2_y_label = 'M2: Location, ticks'  # Motor 2, y-axis title
 
 with serial.Serial('COM11',115200) as s_port: # Fill in correct serial port when found
     s_port.flush() # Clear serial port
@@ -35,7 +35,7 @@ with serial.Serial('COM11',115200) as s_port: # Fill in correct serial port when
         try:    # Check if the values can be converted into a float
             entry[1] = float(entry[1])
             entry[2] = float(entry[2])
-            if entry[0] == 'Motor 1':
+            if entry[0] == 'Motor 1': # Determine which motor wrote
                 mot_1_x_data.append(entry[1])
                 mot_1_y_data.append(entry[2])
             elif entry[0] == 'Motor 2':
@@ -45,7 +45,7 @@ with serial.Serial('COM11',115200) as s_port: # Fill in correct serial port when
             pass
 
     
-# Print values into new plot
+# Print values for both motors into a supblot
 fig, axs = pyplot.subplots(2)
 fig.suptitle('Motor Response')
 
